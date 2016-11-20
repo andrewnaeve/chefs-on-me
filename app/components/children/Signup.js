03ba23cb8cc0040ad1e720d37972e181bd2bb2ad
Drake Smith <drakesmithjr@gmail.com>
2016-11-19 19:05:16 -0800

    react SignUp, need to push to database

--- app/components/children/Signup.js
+++ app/components/children/Signup.js
@@ -7,47 +7,49 @@ var Signup = React.createClass({
 
 	},
 	onButtonClick: function(e){
+		//console.log("E: " + e);
 		e.preventDefault();
+		//console.log(this.refs.name.value);
+		console.log(this.refs.dinerOrChef.value);
+		var namebro = this.refs.name.value;
+		var email = this.refs.email.value;
+		var password = this.refs.password.value;
+		var address = this.refs.address.value;
+		var dinerOrChef = this.refs.dinerOrChef.value;
+		//console.log(namebro);
 
+		//console.log(name + ' ' + email + ' ' + password + ' ' + address + ' ' + dinerOrChef);
 	},
-
 
 	//render component
 	render: function(){
 		return(
-				<div className="row">
+			<div className="row">
+			    <h2>Sign Up!</h2>
+			    <form onSubmit={this.onButtonClick} className="create-update-form" action="/create/user" method="POST">
+			        <div className="form-group">
+			            <label htmlFor="name">Name</label>
+			            <input type="text" className="form-control" id="name" aria-describedby="Name" placeholder="Full Name" ref="name" />
+			        </div>
+			        <div className="form-group">
+			            <label htmlFor="email">Email address</label>
+			            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" ref="email" />
 
-					<h2>Sign Up!</h2>
-          <form onSubmit={this.onButtonClick} className="create-update-form" action="/add" method="POST">
+			        </div>
+			        <div className="form-group">
+			            <label htmlFor="password">Password</label>
+			            <input type="password" className="form-control" id="password" placeholder="Password" ref="password" />
+			        </div>
+			        <div className="radio">
+			        	<label><input type="radio" name="chefOrDiner" ref="dinerOrChef" value="Chef" />Chef</label>
+			        </div>
+			     	<div className="radio">
+			        	<label><input type="radio" name="chefOrDiner" ref="dinerOrChef" value="Diner" />Diner</label>
+			        </div>
 
-						<div className="form-group">
-							<label htmlFor="exampleInputEmail1">Email address</label>
-							<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-							</input>
-						</div>
-						<div className="form-group">
-              <label htmlFor="exampleInputPassword1">Password</label>
-              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
-            </div>
-            <fieldset className="form-group">
-              <legend>Chef or Diner</legend>
-              <div className="form-check">
-                <label className="form-check-label">
-                  <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios1" value="option1"></input>
-                  Are you a chef?
-                </label>
-              </div>
-              <div className="form-check">
-                <label className="form-check-label">
-                  <input type="radio" className="form-check-input" name="optionsRadios" id="optionsRadios2" value="option2"></input>
-                  Are you a diner?
-                </label>
-              </div>
-            </fieldset>
-            <button type="submit" className="btn btn-primary">Submit</button>
-					</form>
-
-				</div>
+			        <button type="submit" className="btn btn-primary">Submit</button>
+			    </form>
+			</div>
 		)
 	}
 });
