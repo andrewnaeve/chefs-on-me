6834319dc3e77148f402b9c28cea0a86de279f0e
andrewnaeve <andrewnaeve@gmail.com>
2016-11-19 16:28:13 -0800

    merge

--- app/components/Main.js
+++ app/components/Main.js
@@ -5,7 +5,15 @@ var Signup = require('./children/Signup.js');
 var Main = React.createClass({
 
 	getInitialState: function () {
-		return { searchTerm: '', zipCode: '' }
+		return { searchTerm: '' }
+	},
+
+	updateSearch: function (newTerm) {
+
+		this.setState({
+			searchTerm: newTerm,
+		});
+
 	},
   
 	//render component
@@ -13,33 +21,53 @@ var Main = React.createClass({
 		return(
 			<div className="container">
 
-        <div className="row">
-          <div className="collapse navbar-collapse navbar-header navbar-right navHeaderCollapse">
-            <ul className="nav navbar-nav navbar-right" >
-              <li><a href ="#about" data-toggle="modal">About</a>
-              </li>
-              <li><a href="#trade" data-toggle="modal"
-                  >Tutorial</a>
-              </li>
-              <li><a href="#add" data-toggle="modal"
-                  >BBBBBBBBB</a>
-              </li>
-              <li><a href="#signup" data-toggle="modal">Sign Up</a>
-              </li>
-              <li><a href="#login" data-toggle="modal">Login</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+				<div className="row">
+				<div className="collapse navbar-collapse navbar-header navbar-right navHeaderCollapse">
+					<ul className="nav navbar-nav navbar-right" >
+					<li><a href ="#about" data-toggle="modal">About</a>
+					</li>
+					<li><a href="#trade" data-toggle="modal"
+						>Tutorial</a>
+					</li>
+					<li><a href="#add" data-toggle="modal"
+						>BBBBBBBBB</a>
+					</li>
+					<li><a href="#signup" data-toggle="modal">Sign Up</a>
+					</li>
+					<li><a href="#login" data-toggle="modal">Login</a>
+					</li>
+					</ul>
+				</div>
+				</div>
 
-        <div className="row">
-          <div className="jumbotron">
-            <h1>Chefs On Me</h1>
-          </div>
-        </div>
-      </div>
-		)
-	}
+				<div className="row">
+				<div className="jumbotron">
+					<h1>Chefs On Me</h1>
+				</div>
+				</div>
+			</div>
+
+			<div className="container">
+
+				<div className="row">
+					<div className="jumbotron">
+						<h1>Chefs On Me</h1>
+					</div>
+				</div>
+				<div className="row">
+					<Search onChange={this.updateSearch}/>
+				</div>
+				<div className="row">
+					<Display searchTerm={this.state.searchTerm}/>
+				</div>
+
+
+			</div>
+
+
+
+
+
 });
 
 
