781ef0228d016a29777bbe1cc1f5fc73058d259e
andrewnaeve <andrewnaeve@gmail.com>
2016-11-19 16:20:49 -0800

    more react

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,40 +1,42 @@
 // Include React 
 var React = require('react');
-// Include React 
-var React = require('react');
 var Search = require('./children/Search.js');
+var Display = require('./children/Display.js');
 
 var Main = React.createClass({
 
 	getInitialState: function () {
-		return { searchTerm: '', zipCode: '' }
+		return { searchTerm: '' }
 	},
 
-	updateSearch: function (newTerm, newZip) {
+	updateSearch: function (newTerm) {
+
 		this.setState({
 			searchTerm: newTerm,
-			zipCode: newZip
 		});
+
 	},
 
-
-
-var Main = React.createClass({
-
-
-	//render component
 	render: function(){
 		return(
 			<div className="container">
+
 				<div className="row">
 					<div className="jumbotron">
 						<h1>Chefs On Me</h1>
 					</div>
 				</div>
-
+				<div className="row">
+					<Search onChange={this.updateSearch}/>
+				</div>
+				<div className="row">
+					<Display searchTerm={this.state.searchTerm}/>
+				</div>
 
 
 			</div>
+
+
 
 
 		)
